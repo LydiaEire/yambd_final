9e51bb95377c1763090449c048535efa188b44c6
Your Name <you@example.com>
2021-08-09 20:14:45 +0300

    adding drf plugin

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import TemplateView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Yamdb API",
+      default_version='v1',
+      description="Документация для приложения проекта Yamdb",
+      # terms_of_service="URL страницы с пользовательским соглашением",
+      contact=openapi.Contact(email="admin@yamdb.ru"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +27,13 @@ urlpatterns = [
          name='redoc'),
     path('api/', include('title_api.urls')),
     path('api/', include('users_api.urls')),
+]
+
+urlpatterns += [
+   url(r'^swagger(?P<format>\.json|\.yaml)$',
+       schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+       name='schema-swagger-ui'),
+   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+       name='schema-redoc'),
 ]
