25829cd99fb15973f0381d033cdea4b2ca33d3f4
Your Name <you@example.com>
2021-08-09 22:33:05 +0300

    urls fixes

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -2,30 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import TemplateView
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from django.conf.urls import url
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Yamdb API",
-        default_version='v1',
-        description="Документация для приложения проекта Yamdb",
-        contact=openapi.Contact(email="admin@yamdb.ru"),
-        license=openapi.License(name="BSD License"),
-    ),
-    permission_classes=(permissions.AllowAny,),
-    public=True,
-)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('redoc/', TemplateView.as_view(template_name='redoc.html'),
          name='redoc'),
     path('api/', include('title_api.urls')),
     path('api/', include('users_api.urls')),
-]
-urlpatterns += [
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
 ]
