5cf76a6b16e957b8199e41f7f55ee9d03358faf8
Your Name <you@example.com>
2021-08-11 11:46:28 +0300

    fixes for 2 review

--- title_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TitleApiConfig(AppConfig):
-    name = 'title_api'
--- title_api/utils.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-from rest_framework.exceptions import ValidationError
-
-
-def year_validator(value):
-    if value < 1900 or value > datetime.datetime.now().year:
-        raise ValidationError('It\'s is not a correct year!')
